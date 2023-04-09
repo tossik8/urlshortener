e9e67f1152a1105e26b7ddff8793c10aa990a568
unknown <toropov.nikita2004@gmail.com>
2023-04-09 10:14:29 +0200

    fix: fix import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const dns = require("node:dns");
+const dns = require("dns");
 const app = express();
 const bodyParser = require("body-parser");
 
